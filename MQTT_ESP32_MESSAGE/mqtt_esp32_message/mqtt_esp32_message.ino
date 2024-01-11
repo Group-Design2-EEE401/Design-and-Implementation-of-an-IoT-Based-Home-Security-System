2d27c93aee4802cbef856470e0e21f056ef82559
computer world <faradayotuoniyo@gmail.com>
2024-01-11 13:12:02 +0100

    fixed echo of messages

--- MQTT_ESP32_MESSAGE/mqtt_esp32_message/mqtt_esp32_message.ino
+++ MQTT_ESP32_MESSAGE/mqtt_esp32_message/mqtt_esp32_message.ino
@@ -7,15 +7,20 @@ const char *password = "maxkkott2018";  // Enter Wi-Fi password
 
 // MQTT Broker
 const char *mqtt_broker = "broker.emqx.io";
-const char *topic = "emqx/esp32";
+const char *topic = "esp32/group_design_send";
+const char *topic_ = "esp32/group_design_receive";
 const char *mqtt_username = "emqx";
 const char *mqtt_password = "public";
 const int mqtt_port = 1883;
 
+int Light = 13;
+String client_id = "esp32-client-";
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 void setup() {
+    
+    pinMode(Light,OUTPUT);
     // Set software serial baud to 115200;
     Serial.begin(115200);
     // Connecting to a WiFi network
@@ -42,13 +47,18 @@ void setup() {
     }
     // Publish and subscribe
     client.publish(topic, "Hi, I'm ESP32 ^^");
-    client.subscribe(topic);
+    client.subscribe(topic_);
+    digitalWrite(Light,HIGH);
+    
 }
 
 void callback(char *topic, byte *payload, unsigned int length) {
     Serial.print("Message arrived in topic: ");
     Serial.println(topic);
     Serial.print("Message:");
+
+    // Check if the message is from the same client
+
     for (int i = 0; i < length; i++) {
         Serial.print((char) payload[i]);
     }
@@ -58,5 +68,9 @@ void callback(char *topic, byte *payload, unsigned int length) {
 
 void loop() {
     client.loop();
-    client.publish(topic, "Hi, I'm ESP32 ^^");
+    if (digitalRead(Light)==HIGH){
+      client.publish(topic, "LIGHT OFF");      
+    }
+
+    delay(6000);
 }
